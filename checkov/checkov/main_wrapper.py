1f31e2dbf00eb708f05021afe4d25d9d78718676
James Chung <james@chung.contact>
2023-06-26 13:55:01 -0500

    update bin

--- checkov/checkov/main_wrapper.py
+++ checkov/checkov/main_wrapper.py
@@ -4,16 +4,6 @@ from main import Checkov
 
 class CheckovWrapper(Checkov):
     def __init__(self, argv: list[str] = sys.argv[1:]):
-        # TODO
         print("Hello from CheckovWrapper")
         print(argv)
         super().__init__(argv)
-
-
-def main():
-    checkov = CheckovWrapper()
-    checkov.run()
-
-
-if __name__ == "__main__":
-    main()
