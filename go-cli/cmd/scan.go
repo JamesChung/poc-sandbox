5a1e78ba759829bf93a36075d95e9a6e959be75c
James Chung <james@chung.contact>
2023-06-30 05:57:10 -0500

    update

--- go-cli/cmd/scan.go
+++ go-cli/cmd/scan.go
@@ -1,22 +1,28 @@
 package cmd
 
 import (
+	"bufio"
+	"encoding/json"
 	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"os/exec"
+	"strings"
+	"sync"
+	"time"
 
 	"github.com/spf13/cobra"
 	"github.com/spf13/pflag"
 )
 
 func setScanPersistentFlags(flags *pflag.FlagSet) {
+	cwd, _ := os.Getwd()
+
 	flags.String(
-		"one",
-		"",
-		"one",
-	)
-	flags.String(
-		"two",
-		"",
-		"two",
+		"directory",
+		cwd,
+		"IaC root directory",
 	)
 }
 func NewScanCommand() *cobra.Command {
@@ -30,27 +36,95 @@ func NewScanCommand() *cobra.Command {
 }
 
 func scanRun(cmd *cobra.Command, args []string) {
-	fmt.Println("hello from scan")
+	directory, err := cmd.Flags().GetString("directory")
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 
-	// checkov := exec.Command("checkov", args...)
-	// stdout, err := checkov.StdoutPipe()
-	// if err != nil {
-	// 	os.Exit(1)
-	// }
+	policies, err := getPolicies()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 
-	// defer stdout.Close()
+	policyNames := make([]string, 0, len(policies))
+	for _, p := range policies {
+		policyNames = append(policyNames, p.Name)
+	}
 
-	// wg := sync.WaitGroup{}
-	// wg.Add(1)
-	// go func() {
-	// 	defer wg.Done()
-	// 	scanner := bufio.NewScanner(stdout)
-	// 	for scanner.Scan() {
-	// 		fmt.Println(scanner.Text())
-	// 	}
-	// }()
+	checkov := exec.Command(
+		"checkov",
+		"--directory",
+		directory,
+		"--check",
+		strings.Join(policyNames, ","),
+	)
 
-	// checkov.Start()
-	// checkov.Wait()
-	// wg.Wait()
+	stdout, err := checkov.StdoutPipe()
+	if err != nil {
+		os.Exit(1)
+	}
+
+	defer stdout.Close()
+
+	wg := sync.WaitGroup{}
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		scanner := bufio.NewScanner(stdout)
+		for scanner.Scan() {
+			fmt.Println(scanner.Text())
+		}
+	}()
+
+	checkov.Start()
+	checkov.Wait()
+	wg.Wait()
+}
+
+type PoliciesResponse struct {
+	Policies []Policy `json:"policies"`
+	Metadata Metadata `json:"metadata"`
+}
+
+type Metadata struct {
+	TransactionID string `json:"transaction_id"`
+}
+
+type Policy struct {
+	Name     string `json:"name,omitempty"`
+	Version  string `json:"version,omitempty"`
+	Location string `json:"location,omitempty"`
+}
+
+func getPolicies() ([]Policy, error) {
+	var protocol = "http"
+	var host = "localhost"
+	var port = "8080"
+	var slug = "policies"
+	var version = "v1"
+
+	client := http.Client{
+		Timeout: time.Second * 3,
+	}
+
+	res, err := client.Get(
+		fmt.Sprintf("%s://%s:%s/%s/%s", protocol, host, port, version, slug))
+	if err != nil {
+		return nil, err
+	}
+
+	resBody, err := io.ReadAll(res.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	policiesResponse := PoliciesResponse{}
+	err = json.Unmarshal(resBody, &policiesResponse)
+	if err != nil {
+		return nil, err
+	}
+
+	return policiesResponse.Policies, nil
 }
